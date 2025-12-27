c54fe17419c067de158336ebfb46133566c0d8ca
yruge <jasonjthen@gmail.com>
2025-12-28 02:24:47 +0700

    added project ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,43 @@
-import Image from "next/image";
-
+import './globals.css'
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main>
+      <div className="hero-layout">
+        <h1 className='hero-intro'>Cooked</h1>
+        <div className="hero-intro-1">
+          <h2>AI Powered Judgement</h2>
+          <p>Your Spotify Wrapped was a lie. Let our AI analyze your listening history and tell you the harsh truth about who you really are.</p>
+          <button>Roast Me</button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+
+      <div className="wrapper">
+        <div className="scrolling-text">
+          <div className="scrolling-text-content">
+            <h1>WARNING: EMOTIONAL DAMAGE AHEAD</h1><span>•</span >
+            <h1>NO REFUNDS ON EGO</h1><span>•</span>
+            <h1>AI IS WATCHING</h1><span>•</span>
+            <h1>YES, WE SAW THE 10-HOUR RAIN SOUNDS</h1><span>•</span>
+          </div>
+
+          <div className="scrolling-text-content">
+            <h1>WARNING: EMOTIONAL DAMAGE AHEAD</h1><span>•</span >
+            <h1>NO REFUNDS ON EGO</h1><span>•</span>
+            <h1>AI IS WATCHING</h1><span>•</span>
+            <h1>YES, WE SAW THE 10-HOUR RAIN SOUNDS</h1><span>•</span>
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+
+
+      <div className="hero-layout-1">
+        <img src="/images/nigor.jpg" alt="Tyler The Creator Funny Album" width="185px" height="187px" style={{right:'20%', top:'5%'}}/>
+        <img src="/images/drake-pregnant.jpg" alt="Drake Funny Album 1" className="drake-pregnant" width="180px" height="180px" style={{left:'40%', top:'20%'}} />
+        <img src="/images/tame-impala.jpg" alt="Tame Impala Funny Album" width="242px" height="229px" style={{top:'10%', left:'1%'}}/>
+        <img src="/images/drake-funny-album.jpg" alt="Drake Funny Album 2" width="280px" height="auto" style={{bottom:'15%', right:'27%'}}/>
+          <img src="/images/snoop-dogg.jpg" alt="Snoop Dogg Funny Picture" className="snoop-dogg" width="177px" height="177px" style={{bottom:'10%', left:'20%'}}/>
+        <img src="/images/drake-pooped.jpg" alt="Drake Funny Album 3" className="drake-pooped" width="179px" height="143px" style={{bottom:'20%', right:'10%'}}/>
+      </div>
+    </main>
   );
 }
